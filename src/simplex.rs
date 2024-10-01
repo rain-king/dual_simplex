d32a04cd46a0afeb1f9014483f1f41099ce304c7
Gustavo <43526958+rain-king@users.noreply.github.com>
2024-10-01 16:07:02 -0700

    replaced main with dual_problem branch, and resolved conflicts with initial commit

--- src/simplex.rs
+++ src/simplex.rs
@@ -14,9 +14,12 @@ pub fn dual_simplex(maximize: bool, c: &matrix<f64>, a: &matrix<f64>, b: &matrix
 	pretty_print_array2(&tableau);
 	println!();
 
-	tableau = initialize(maximize, &c, &a, &b);
+	let dual_problem: bool;
+	(tableau, dual_problem) = initialize(maximize, &c, &a, &b);
 	
-	basis = iterations(maximize, &mut tableau);
+	dbg!(dual_problem);
+	
+	basis = iterations(&mut tableau);
 	println!("The final tableau is:");
 	pretty_print_array2(&tableau);
 	println!();
@@ -24,7 +27,7 @@ pub fn dual_simplex(maximize: bool, c: &matrix<f64>, a: &matrix<f64>, b: &matrix
 	basis
 }
 
-fn iterations(maximize: bool, tableau: &mut matrix<f64>) -> Vec<(usize, usize)> {
+fn iterations(tableau: &mut matrix<f64>) -> Vec<(usize, usize)> {
 	let mut basis = initialize_basis(tableau.to_owned());
 
 	let mut iteration = 0;
@@ -33,18 +36,18 @@ fn iterations(maximize: bool, tableau: &mut matrix<f64>) -> Vec<(usize, usize)> 
 		pretty_print_array2(&tableau);
 		println!();
 		
-		if iteration == 5 {
-			panic!();
-		}
-		
-		let (pivot_row_index, pivot_column_index) = pivot(tableau, maximize, &basis);
+		let (pivot_row_index, pivot_column_index) = pivot(tableau, &basis);
 		for element in basis.iter_mut() {
 			// variable with pivot row enters, variable with pivot column exits
 			if element.0 == pivot_row_index {
 				*element = (pivot_row_index, pivot_column_index);
 			}
 		}
-
+		
+		if iteration == 5 {
+			break;
+		}
+		
 		iteration += 1;
 	}
 
@@ -74,12 +77,12 @@ fn is_basic(column: vector<f64>) -> bool {
 	has_only_one_1 && everything_else_is_0
 }
 
-fn not_feasible(tableau: &mut matrix<f64>) -> bool {
-	tableau.column(tableau.ncols() - 1).slice(s![1..]).into_iter().any(|&x| x < 0.0)
+fn not_feasible(tableau: &matrix<f64>) -> bool {
+	tableau.column(tableau.ncols() - 1).slice(s![1..]).iter().any(|&x| x < 0.0)
 }
 
-fn pivot(tableau: &mut matrix<f64>, maximize: bool, basis: &Vec<(usize, usize)>) -> (usize, usize) {
-	let (pivot_row_index, pivot_column_index) = pivot_indexes(tableau, maximize, basis);
+fn pivot(tableau: &mut matrix<f64>, basis: &Vec<(usize, usize)>) -> (usize, usize) {
+	let (pivot_row_index, pivot_column_index) = pivot_indexes(tableau, basis);
 
 	let normalization_scalar = tableau[(pivot_row_index, pivot_column_index)].to_owned();
 	
@@ -96,23 +99,26 @@ fn pivot(tableau: &mut matrix<f64>, maximize: bool, basis: &Vec<(usize, usize)>)
 	(pivot_row_index, pivot_column_index)
 }
 
-fn pivot_indexes(tableau: &mut matrix<f64>, maximize: bool, basis: &Vec<(usize, usize)>) -> (usize, usize) {
-	let pivot_row_index = argmin(&tableau.column(tableau.ncols() - 1).slice(s![1..]).to_owned()) + 1;
+fn pivot_indexes(tableau: &mut matrix<f64>, basis: &Vec<(usize, usize)>) -> (usize, usize) {
+	let pivot_row_index = argmin(&tableau.column(tableau.ncols() -1).slice(s![1..]).to_owned()) + 1;
+	
+	if tableau.row(pivot_row_index).slice(s![0..-1]).iter().all(|&x| x >= 0.0) {
+		panic!("!!! The problem has no feasible solution.")
+	}
+	
 	let mut basis_cols = basis.iter().map(|x| x.1).into_iter();
 
-	let mut pivot_column_index = 0 as usize;
-	let mut optimal_quotient = if maximize { f64::INFINITY } else { f64::NEG_INFINITY };
-	for (j, pivot_value) in tableau.row(pivot_row_index).into_iter().enumerate() {
+	let mut pivot_column_index = 0;
+	let mut optimal_quotient = if true {f64::INFINITY} else {f64::NEG_INFINITY};
+	for (j, &pivot_value) in tableau.row(pivot_row_index).into_iter().enumerate() {
 		if j < tableau.ncols() - 1 && !(basis_cols.any(|c| c == j)) {
 			let z_j_minus_c_j = tableau[(0, j)];
-			if *pivot_value < 0.0 {
-				let quotient_condition = if maximize {
-					(z_j_minus_c_j / pivot_value).abs() < optimal_quotient 
-				} else {
-					(z_j_minus_c_j / pivot_value).abs() > optimal_quotient
-				};
-				if quotient_condition {
-					optimal_quotient = z_j_minus_c_j / pivot_value;
+			let pivot_column_condition = pivot_value < 0.0;
+			
+			if pivot_column_condition {
+				let current_quotient_abs = (z_j_minus_c_j / pivot_value).abs();
+				if current_quotient_abs < optimal_quotient {
+					optimal_quotient = current_quotient_abs;
 					pivot_column_index = j;
 				}
 			}
@@ -126,24 +132,24 @@ fn argmin(arr: &vector<f64>) -> usize {
 	let mut min = f64::INFINITY;
 	let mut argmin: usize = 0;
 
-	for (i, value) in arr.into_iter().enumerate() {
-		if *value < min {
-			min = *value;
+	for (i, &value) in arr.into_iter().enumerate() {
+		if value < min {
+			min = value;
 			argmin = i;
 		}
 	}
 	argmin
 }
 
-fn argmax(arr: &vector<f64>) -> usize {
-	let mut max = -f64::INFINITY;
-	let mut argmax: usize = 0;
+// fn argmax(arr: &vector<f64>) -> usize {
+// 	let mut max = -f64::INFINITY;
+// 	let mut argmax: usize = 0;
 
-	for (i, value) in arr.into_iter().enumerate() {
-		if *value > max {
-			max = *value;
-			argmax = i;
-		}
-	}
-	argmax
-}
+// 	for (i, &value) in arr.into_iter().enumerate() {
+// 		if value > max {
+// 			max = value;
+// 			argmax = i;
+// 		}
+// 	}
+// 	argmax
+// }
