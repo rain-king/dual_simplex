7ffca25ed6ac0ae321627dc0a8730bb21c9b7cb6
Gustavo <43526958+rain-king@users.noreply.github.com>
2024-10-01 15:36:57 -0700

    tests in examples passed, changed input format

--- src/simplex/preparation.rs
+++ src/simplex/preparation.rs
@@ -3,7 +3,7 @@ use ndarray::{concatenate, Array2 as matrix, Axis};
 
 
 pub fn original_tableau(maximize: bool, c: &matrix<f64>, a: &matrix<f64>, b: &&matrix<f64>) -> matrix<f64> {
-	let tableau_bottom = get_tableu_bottom(a, b);
+	let tableau_bottom = get_tableu_bottom(a, b, false);
 
 	let tableau_top = concatenate![
 		Axis(1),
@@ -17,20 +17,38 @@ pub fn original_tableau(maximize: bool, c: &matrix<f64>, a: &matrix<f64>, b: &&m
 	concatenate![Axis(0), tableau_top, tableau_bottom]
 }
 
-pub fn initialize(maximize: bool, objective: &matrix<f64>, a: &matrix<f64>, b: &matrix<f64>) -> matrix<f64> {
-	let tableau_bottom = get_tableu_bottom(&a, &b);
+pub fn initialize(maximize: bool, c: &matrix<f64>, a: &matrix<f64>, b: &matrix<f64>) -> (matrix<f64>, bool) {
 	let tableau_top: matrix<f64>;
+	let tableau_bottom: matrix<f64>;
+	let dual_problem = b.column(0).iter().all(|&x| x >= 0.0);
+	if dual_problem {
+		let c_transposed = c.t().to_owned();
+		let b_transposed = b.t();
+		tableau_bottom = get_tableu_bottom(&a, &c_transposed, true);
+		tableau_top = concatenate![
+			Axis(1),
+			// has to start optimal in minimization or maximization
+			if !maximize { b_transposed.to_owned() } else { -b_transposed.to_owned() }, 
+			matrix::zeros((1, tableau_bottom.ncols() - b_transposed.ncols()))
+		];
+	} else {
+		tableau_bottom = get_tableu_bottom(&a, &b, false);
+		tableau_top = concatenate![
+			Axis(1),
+			if maximize { -(*c).to_owned() } else { (*c).to_owned() },
+			matrix::zeros((1, tableau_bottom.ncols() - c.ncols()))
+		];
+	}
 
-	tableau_top = concatenate![
-		Axis(1),
-		if maximize { -objective.clone() } else { objective.clone() },
-		matrix::zeros((1, tableau_bottom.ncols() - objective.ncols()))
-	];
-
-	 concatenate![Axis(0), tableau_top, tableau_bottom]
+	 (concatenate![Axis(0), tableau_top, tableau_bottom], dual_problem)
 }
 
-pub fn get_tableu_bottom(a: &matrix<f64>, b: &matrix<f64>) -> matrix<f64> {
-	let bottom_left = &concatenate![Axis(1), *a, matrix::eye(a.nrows())];
-	concatenate![Axis(1), *bottom_left, b.to_owned()]
+pub fn get_tableu_bottom(a: &matrix<f64>, b: &matrix<f64>, transpose: bool) -> matrix<f64> {
+	if transpose {
+		let bottom_left = &concatenate![Axis(1), (*a).t(), -matrix::eye(a.ncols())];
+		concatenate![Axis(1), -(*bottom_left).to_owned(), -(*b).to_owned()]
+	} else {
+		let bottom_left = &concatenate![Axis(1), *a, matrix::eye(a.nrows())];
+		concatenate![Axis(1), *bottom_left, *b]
+	}
 }
